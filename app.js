b4d56497b9f0286e01912ff7f7ef15681fa5b13a
brands <brandonlee.swanepoel@gmail.com>
2020-06-10 11:05:49 +0200

    Added sql to notification email

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = config.port;
 const connection = config.connection;
 
 //Middleware
-app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(flash())
 
@@ -51,17 +51,17 @@ app.use(require('./routes/likes.js'));
 app.use(require('./routes/logout.js'));
 app.use(require('./routes/reportuser.js'));
 
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
     res.locals.success_msg =
-    req.flash('success_msg');
+        req.flash('success_msg');
     res.locals.error_msg =
-    req.flash('error_msg');
+        req.flash('error_msg');
     res.locals.error =
-    req.flash('error');
+        req.flash('error');
     next();
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -72,63 +72,63 @@ app.use(function(err, req, res, next) {
 });
 
 //Connect to DB mysql
-connection.connect(function(error){
-    if(!!error){
+connection.connect(function (error) {
+    if (!!error) {
         console.log('ERROR');
-    }else{
+    } else {
 
         console.log('MySQL Connected...');
     }
 });
 //create db
-app.get('/createDatabase',(req,res)=>{
+app.get('/createDatabase', (req, res) => {
     let sql = "CREATE DATABASE Matcha";
-    connection.query(sql, (err, result)=>{
-        if(err) throw err;
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
         console.log(result);
         res.send("Database created...")
     })
 })
 //create User Table
-app.get('/createTable',(req,res)=>{
+app.get('/createTable', (req, res) => {
     let sql = "CREATE TABLE users(user_id INT AUTO_INCREMENT,name VARCHAR(100), surname VARCHAR(100),username VARCHAR(100),email VARCHAR(100), password VARCHAR(100),age INT, gender VARCHAR(100), sp VARCHAR(100), bio VARCHAR(100), verified BOOLEAN DEFAULT false,status VARCHAR(100) DEFAULT 'offline' ,image LONGTEXT, vkey LONGTEXT, sport VARCHAR(10) DEFAULT 'off',fitness VARCHAR(10) DEFAULT 'off',technology VARCHAR(10) DEFAULT 'off', music VARCHAR(10) DEFAULT 'off', gaming VARCHAR(10) DEFAULT 'off', ageBetween VARCHAR(20), city VARCHAR(200), country VARCHAR(200), postal VARCHAR(200),PRIMARY KEY (user_id))";
-    connection.query(sql, (err, result)=>{
-        if(err) throw err;
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
         console.log("Users Table created...");
     })
     let galleryTableSql = "CREATE TABLE gallery(ID INT AUTO_INCREMENT ,user_id INT not null references users(user_id), gallery LONGTEXT, PRIMARY KEY (ID))";
-    connection.query(galleryTableSql, (err, result)=>{
-        if(err) throw err;
+    connection.query(galleryTableSql, (err, result) => {
+        if (err) throw err;
         console.log("gallery Table created...");
     })
     let likedTableSql = "CREATE TABLE liked(ID INT AUTO_INCREMENT ,user_id INT not null references users(user_id), username VARCHAR(100), PRIMARY KEY (ID))";
-    connection.query(likedTableSql, (err, result)=>{
-        if(err) throw err;
+    connection.query(likedTableSql, (err, result) => {
+        if (err) throw err;
         console.log("Liked Table created...");
     })
     let likedByTableSql = "CREATE TABLE likedBy (ID INT AUTO_INCREMENT ,user_id INT not null references users(user_id), username VARCHAR(100), PRIMARY KEY (ID))";;
-    connection.query(likedByTableSql, (err, result)=>{
-        if(err) throw err;
+    connection.query(likedByTableSql, (err, result) => {
+        if (err) throw err;
         console.log("LikedBy Table created...");
     })
     let viewedByTableSql = "CREATE TABLE viewedBy (ID INT AUTO_INCREMENT , user_id INT not null references users(user_id), username VARCHAR(100), PRIMARY KEY (ID))";
-    connection.query(viewedByTableSql, (err, result)=>{
-        if(err) throw err;
+    connection.query(viewedByTableSql, (err, result) => {
+        if (err) throw err;
         console.log("viewedBy Table created...");
     })
     let viewedProfileHistoryTableSql = "CREATE TABLE viewedProfileHistory (ID INT AUTO_INCREMENT ,user_id INT not null references users(user_id), username VARCHAR(100), PRIMARY KEY (ID))";
-    connection.query(viewedProfileHistoryTableSql, (err, result)=>{
-        if(err) throw err;
+    connection.query(viewedProfileHistoryTableSql, (err, result) => {
+        if (err) throw err;
         console.log("viewedProfileHistoryTableSql Table created...");
     })
     let chatTableSql = "CREATE TABLE chat(ID INT AUTO_INCREMENT ,chatId VARCHAR(100),ToUser VARCHAR(100), fromUser VARCHAR(100),msg LONGTEXT,created VARCHAR(100), PRIMARY KEY (ID))";
-    connection.query(chatTableSql, (err, result)=>{
-        if(err) throw err;
+    connection.query(chatTableSql, (err, result) => {
+        if (err) throw err;
         console.log("chat Table created...")
     })
     let blockedTableSql = "CREATE TABLE blocked(ID INT AUTO_INCREMENT , user_id INT not null references users(user_id), username VARCHAR(100), PRIMARY KEY (ID))";
-    connection.query(blockedTableSql, (err, result)=>{
-        if(err) throw err;
+    connection.query(blockedTableSql, (err, result) => {
+        if (err) throw err;
         console.log("Blocked Table created...");
     })
     res.send("Database Tables Created...")
@@ -145,53 +145,60 @@ function saveMsg(data) {
 };
 
 function notif_mail(from, to, notif) {
-    schema.user.findOne({ username: to }, async function (err, data) {
+    var userEmail;
+    let userInfoSql = `SELECT * FROM users WHERE username = '${to}'`;
+    connection.query(userInfoSql, async (err, result) => {
         if (err) throw err;
-
-        app.mailer.send('notif', {
-            to: data.email,
-            subject: 'Matcha - Notification from ' + from,
-            from: from,
-            notif: notif
-        }, function (err) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log('Notification email sent to ' + from);
-        })
-})}
+        if (result) {
+            result.forEach(function (result) {
+                userEmail = result.email
+            })
+            app.mailer.send('notif', {
+                to: userEmail,
+                subject: 'Matcha - Notification from ' + from,
+                from: from,
+                notif: notif
+            }, function (err) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('Notification email sent to ' + to);
+            })
+        }
+    })
+}
 
 //Socket setup
 var io = socket(server);
-io.on('connection',function(socket){
-    socket.on('chat',function(data){
+io.on('connection', function (socket) {
+    socket.on('chat', function (data) {
         socket.join(data.chatId);
-        io.sockets.to(data.chatId).emit('chat',data);
+        io.sockets.to(data.chatId).emit('chat', data);
         saveMsg(data)
-        io.sockets.to(data.to).emit('msg_notification',data.from);
+        io.sockets.to(data.to).emit('msg_notification', data.from);
         console.log('Message added to DB!')
         notif_mail(data.from, data.to, " has sent you a message!")
     });
-    socket.on('liked',(data)=>{
-        io.sockets.to(data.to).emit('like_notification',data.from);
+    socket.on('liked', (data) => {
+        io.sockets.to(data.to).emit('like_notification', data.from);
         console.log('Like notification!')
         notif_mail(data.from, data.to, " has liked your profile!")
     });
-    socket.on('unliked',(data)=>{
-        io.sockets.to(data.to).emit('unlike_notification',data.from);
+    socket.on('unliked', (data) => {
+        io.sockets.to(data.to).emit('unlike_notification', data.from);
         console.log('unLike notification!')
         notif_mail(data.from, data.to, " has unliked your profile!")
     });
-    socket.on('viewed',(data)=>{
-        io.sockets.to(data.to).emit('viewed_notification',data.from);
+    socket.on('viewed', (data) => {
+        io.sockets.to(data.to).emit('viewed_notification', data.from);
         console.log('view notification! from: ' + data.from)
         notif_mail(data.from, data.to, " has viewed your profile!")
     });
-    socket.on('room',function(data){
+    socket.on('room', function (data) {
         socket.join(data);
     })
-    socket.on('notif',function(data){
+    socket.on('notif', function (data) {
         socket.join(data);
     })
 });
